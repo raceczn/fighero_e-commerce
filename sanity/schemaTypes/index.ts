b75bb2f7990868ce9dadfdedc396a642fdbef56d
Rances Cuizon <rancescuizon@gmail.com>
2025-05-22 23:11:49 +0800

    first commit

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,18 @@
+import { type SchemaTypeDefinition } from "sanity";
+import { categoryType } from "./categoryType";
+import { blockContentType } from "./blockContentType";
+import { productType } from "./productType";
+import { orderType } from "./orderType";
+import { brandType } from "./brandTypes";
+import { addressType } from "./addressType";
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [
+    categoryType,
+    blockContentType,
+    productType,
+    orderType,
+    brandType,
+    addressType,
+  ],
+};
