2f0184f713c727f0448fb99d282af2d10b98f6e5
Rances Cuizon <rancescuizon@gmail.com>
2025-06-17 18:19:09 +0800

    refactor: remove unused banner image import from index.ts

--- images/index.ts
+++ images/index.ts
@@ -1,4 +1,3 @@
 import paypalLogo from "./paypalLogo.png";
 import emptyCart from "./emptyCart.png";
-import banner_1 from "./banner/bg.png";
-export { paypalLogo, emptyCart, banner_1 };
+export { paypalLogo, emptyCart };
