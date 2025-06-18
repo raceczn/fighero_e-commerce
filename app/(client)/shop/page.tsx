1d39ab10b185576f4be41a9a6b7ac8271a69480a
Rances Cuizon <rancescuizon@gmail.com>
2025-06-18 23:46:33 +0800

    fix for ESLint error in deployment

--- app/(client)/shop/page.tsx
+++ app/(client)/shop/page.tsx
@@ -1,13 +1,16 @@
 import Shop from "@/components/Shop";
 import { getAllBrands, getCategories } from "@/sanity/queries";
-import React from "react";
+import React, { Suspense } from "react";
 
 const ShopPage = async () => {
   const categories = await getCategories();
   const brands = await getAllBrands();
+
   return (
     <div className="bg-white">
-      <Shop categories={categories} brands={brands} />
+      <Suspense fallback={<div>Loading shop...</div>}>
+        <Shop categories={categories} brands={brands} />
+      </Suspense>
     </div>
   );
 };
